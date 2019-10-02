0123f6ddda5868b648ce320512f38fc4867ffcac
Toño SC <mouzhroq@gmail.com>
2019-10-01 20:19:38 -0500

    Ejercicios

---
+++ p1_ejercicios.py
@@ -0,0 +1,16 @@
+import random 
+
+#Ejercicio 1
+print(random.random(), random.random(), random.random(), random.random(), random.random())
+
+#Ejercicio 2
+print(random.gauss(0, 0.5), random.gauss(0, 0.5), random.gauss(0, 0.5), random.gauss(0, 0.5), random.gauss(0, 0.5))
+
+#Ejercicio 3
+print(random.gauss(-1, 2), random.gauss(-1, 2), random.gauss(-1, 2), random.gauss(-1, 2), random.gauss(-1, 2))
+
+#Ejercicio 4
+print(round(random.gauss(0, 0.5), 4), round(random.gauss(0, 0.5), 4), round(random.gauss(0, 0.5), 4), round(random.gauss(0, 0.5), 4), round(random.gauss(0, 0.5), 4))
+
+#Ejercicio 5
+print(round(random.gauss(-1, 2), 4), round(random.gauss(-1, 2), 4), round(random.gauss(-1, 2), 4), round(random.gauss(-1, 2), 4), round(random.gauss(-1, 2), 4))
