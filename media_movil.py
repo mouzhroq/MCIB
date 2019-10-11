0603e7c052df311df5df45e72d0b98c39af4b30d
Toño SC <mouzhroq@gmail.com>
2019-10-10 22:59:50 -0500

    media movil terminada

--- media_movil.py
+++ media_movil.py
@@ -8,21 +8,21 @@ fig, ax = plt.subplots()
 ax.plot(t,s)
 #plt.show()
 
-l = 100
-t = 25
-x = np.arange(0, len(s)-l, l-1)
-y = np.delete((np.arange(0, len(s)-l, l-1)-(l*(t/100))),0)
-index = np.empty(len(x)+len(y)).astype(int)
-index[::2] = x
-index[1::2] = y
+l = 50
+t = 10
+index=np.array([0, l])
+while index[-1]+l<len(s):
+    index= np.append(index, np.array([index[-1]-l*(t/100), index[-1]-l*(t/100)+l]) )
+index = np.array(np.sort(np.delete(index, -1))).astype(int)
 
 epochs = np.array([s[i:i+l] for i in index])
 m = np.mean(epochs, axis=1)
 ti = epochs-m[:,np.newaxis]
 filt = ti[0,:]
 for i in range (1, ti.shape[0]):
-    filt = np.append(filt, ti[i, int(l*(t/100))::])
+    filt = np.append(filt, ti[i, int(len(filt)-index[i])::])
+filt = np.append(filt, s[index[-1]+l::]-np.mean(s[index[-1]+l::]))
 
-print(filt.shape)
-# mf = np.mean(s[index[-1]::])
-# ti = 
+plt.figure()
+plt.plot(filt)
+plt.show()
